a421f1ca216aeb37ea3732f90348a5829713968a
Arthur V Fain <arthurvfain@gmail.com>
2021-05-20 13:13:09 -0400

    commit tree c81d5d9143080c816559127eee8274772783baba

--- index.js
+++ index.js
@@ -11,6 +11,72 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased()
+{
+  return tutorials.map(title =>
+  {
+    let working = [...title];
+    for (let i = 0; i < working.length-1; i++)
+    {
+       if (i === 0)
+       {
+         working[i] = working[i].toUpperCase();
+       }
+       else if (title[i-1] === " ")
+       {
+         working[i] = working[i].toUpperCase();
+       }
+    }
+    return working.join('');
+  });
 }
+
+// function titleCased(){
+//   let finalOutput = []
+
+//   tutorials.forEach(phrase => {
+//     // break phrase into single words
+//     // capitalize each word
+//     // join words back into phrases
+//     // re-build phrase array with new phrases
+
+//     let phraseWordArray = phrase.split(" ")
+//     let mappedPhraseWords = phraseWordArray.map(word => word[0].toUpperCase())
+//     let newPhraseArray = mappedPhraseWords.join(" ")
+//     finalOutput.push(newPhraseArray)
+//   })
+
+//   return finalOutput
+// }
+
+
+// regex
+
+
+// function titleCase(ars,title)
+// {
+//   let working = [...ars[title]]
+//   //console.log(ars[title]);
+//   for (let i = 0; i < working.length-1; i++)
+//   {
+//      if (i === 0)
+//      {
+//        working[i] = working[i].toUpperCase();
+//        //console.log(working[i]);
+//      }
+//     else if (ars[title][i-1] === " ")
+//     {
+//       working[i] = working[i].toUpperCase();
+//       //console.log(working[i]);
+//     }
+//     //ars[title] = working.join(``);
+//   }
+//   ars[title] = working.join(``);
+//   return ars[title];
+// }
+
+// function titleCased(arr)
+// {
+//   arr.map(titleCase)
+//   return tutorials;
+// }
